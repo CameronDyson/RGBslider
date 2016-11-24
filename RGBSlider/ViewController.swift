4bdc8cf24066d2a825a1841b7f781e1b6c524c78
Cameron Dyson <camerondyson@stuarts-mbp.home>
2016-11-24 13:44:42 +0000

    added menu

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var open: UIButton!
+    @IBOutlet weak var colorSquare: UIView!
+    //@IBOutlet var colorSquare: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateBackgroundColor()
         // Do any additional setup after loading the view, typically from a nib.
+        colorSquare.layer.borderColor = UIColor.black.cgColor
+        colorSquare.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
